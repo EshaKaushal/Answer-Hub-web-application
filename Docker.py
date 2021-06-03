26df511bc77c0d883c31585c852e42cbe6b00969
Esha Kaushal <84419207+EshaKaushal@users.noreply.github.com>
2021-06-03 01:50:03 -0400

    Update Docker.py

--- Docker.py
+++ Docker.py
@@ -3,8 +3,9 @@ FROM python:3.7-slim
 EXPOSE 8080
 ENV APP_HOME /app
 WORKDIR $APP_HOME
-COPY . ./
 
-RUN pip install requests streamlit
+COPY Streamlitapp.py /app/Streamlitapp.py
 
-CMD ["streamlit", "run", "--server.port", "8080", "--server.enableCORS", "false", "Streamlitapp.py"]
+RUN pip install -r requirements.txt
+
+CMD ["streamlit", "run", "--server.port", "8080", "--server.enableCORS", "false", "/app/Streamlitapp.py"]
