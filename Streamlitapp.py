fcad6a3edfc0e2336e7ee54c7129d06c24b70431
Esha Kaushal <84419207+EshaKaushal@users.noreply.github.com>
2021-06-03 16:25:10 -0400

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -7,7 +7,6 @@ import os
 #Get URL from Git env variables
 #url = "https://mgmt590-restapi-es7glm5rsq-uc.a.run.app"
 url = format(os.environ.get('REST_API_URL'))
-print("REST API URL is ",url)
 
 st.title("Question Answering Application")
 
@@ -22,7 +21,7 @@ def getModelList():
         modelList.append(modelResp[i]["name"])
     print("Models received from Database",len(modelResp))
     print(modelList)
-    return modelList
+    return modelList, modelResp
 
 #Section 1 - Add/Delete Model List
 
@@ -45,12 +44,12 @@ if ModelSec.button('Add Model'):
 
     response = requests.request("PUT", AddModelURL, headers=headers, data=payload)
     print(response.text)
-    ModelSec.success('Model has been added')
+    ModelSec.success('Model has been Added')
 
 #Section 2 - Delete a Model
 ModelSec2 = st.beta_expander("Delete a Model")
 ModelSec2.title("Delete a model")
-modelList = getModelList()
+modelList, modelResp = getModelList()
 DelModel = ModelSec2.selectbox('Select Model to delete',modelList)
 if ModelSec2.button('Delete Model'):
     #Trigger Delete request
@@ -64,12 +63,11 @@ if ModelSec2.button('Delete Model'):
 AnswerSec = st.beta_expander("Answer a Question")
 question = AnswerSec.text_input('Question')
 Context = AnswerSec.text_area ('Context')
-modelList2 = getModelList()
-modelList2.append("Default")
+modelList2, modelResp = getModelList()
 ModelName = AnswerSec.selectbox('Select Model',modelList2)
 if AnswerSec.button('Get Answer'):
     #Trigger POST request
-    if (ModelName == None) or (ModelName == "Default"):
+    if ModelName == None:
         AnswerURL = url + "/answer"
     else:
         AnswerURL = url + "/answer?model=" + ModelName
@@ -87,14 +85,15 @@ if AnswerSec.button('Get Answer'):
 
 #Section 3
 BulkUploadSec = st.beta_expander("Bulk Upload Question and Context")
-file = BulkUploadSec.file_uploader("Upload questions and context")
+file = BulkUploadSec.file_uploader("Upload a csv questions and context file")
 if (file is not None) and BulkUploadSec.button('Fetch Answer'):
-    df = pd.read_excel(file)
+    df = pd.read_csv(file)
+    answer_list = []
     print("length of file is ",len(df))
     #print(df)
     for i in range(0,len(df)):
-        question = df["Question"][i]
-        context = df["Context"][i]
+        question = df['question'][i]
+        context = df['context'][i]
         #trigger API to get answer
         AnswerURL = url + "/answer"
         headers = {
@@ -106,18 +105,17 @@ if (file is not None) and BulkUploadSec.button('Fetch Answer'):
         })
         response = requests.request("POST", AnswerURL, headers=headers, data=payload)
         Answer = json.loads(response.text)
+        answer_list.append(Answer["answer"])
         print(Answer["answer"])
-        BulkUploadSec.success("Answer is " + Answer["answer"])
+        #BulkUploadSec.success("Answer is " + Answer["answer"])
+    df['Answer'] = answer_list
+    BulkUploadSec.table(df)
 
 #Sidebar
-sidebar = st.sidebar
-sidebar.title("Available Models in Database")
-modelList = getModelList()
-sidebar.table(modelList)
+# sidebar = st.beta_expander("List of available models")
+# modelList, modelResp = getModelList()
+# sidebar.table(modelResp)
 
-#url = "https://mgmt590-restapi-es7glm5rsq-uc.a.run.app/answer"
-#Column 2 - Available model list
-# modelList = getModelList()
-# col2.title("List of Available Models")
-# col2.table(modelList)
-
+st.title("List of available models")
+modelList, modelResp = getModelList()
+st.table(modelResp)
